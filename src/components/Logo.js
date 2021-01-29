10601cec3352c126014f57b1e1282dc8362ae4fd
DAKACR <71095354+DAKACR@users.noreply.github.com>
2021-01-29 12:13:24 -0800

    Update Logo.js

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,20 +1,19 @@
 import React from "react";
 import styled from "styled-components";
 
-import logo from "assets/img/header/logo.jpg";
-
-const LogoStyled = styled.img`
+const LogoStyled = styled.h1`
   color: #fff;
   width: 90px;
   margin: 0;
   font-family: "Roboto-Bold";
   position: relative;
+  text-decoration: underline;
   &:hover {
     filter: brightness(0.9);
     cursor: pointer;
   }
 `;
 
-export default function Logo() {
-  return <LogoStyled src={logo} alt="logo" />;
+export default function Logo({ children }) {
+  return <LogoStyled>{children}</LogoStyled>;
 }
