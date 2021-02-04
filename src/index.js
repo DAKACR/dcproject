19b67498c396c47b1816a34cf95b4c4b91c8c6ca
DAKACR <71095354+DAKACR@users.noreply.github.com>
2021-02-04 12:47:31 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div style={{ width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <h1 style={{ fontSize: '3rem', fontWeight: '900' }}>SITE UNDER CONSTRUCTION.</h1>
+    </div>
   </React.StrictMode>,
   document.getElementById("root")
 );
